69379a4202ac9193aaccdfc74c3e1f495d5fa79f
Ruzana123 <Ruzana18@bigmir.net>
2016-05-18 14:40:25 +0300

    widgets

--- rz_theme/framework/widgets/widget-currency-converter.php
+++ rz_theme/framework/widgets/widget-currency-converter.php
@@ -34,7 +34,8 @@ public function form( $instance ) {
 // Default widget settings
 	$defaults = array(
 	'title' => 'Currency converter',
-	'currency' => ''
+	'currency' => '',
+	'currency1' => ''
 );
 
 $instance = wp_parse_args( (array) $instance, $defaults );
@@ -46,12 +47,20 @@ $instance = wp_parse_args( (array) $instance, $defaults );
 		<input type="text" name="<?php echo $this->get_field_name( 'title' ); ?>" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" value="<?php echo esc_attr( $instance['title'] ); ?>">
 	</p>
 
-	<!-- City -->
 	<select name="<?php echo $this->get_field_name( 'currency' ); ?>" id="<?php echo $this->get_field_id( 'title' ); ?>">
+		<option <?php selected( $instance['currency'], '' ); ?> >''</option>
 		<option <?php selected( $instance['currency'], 'USD' ); ?> >USD</option>
   		<option <?php selected( $instance['currency'], 'EUR' ); ?> >EUR</option>
   		<option <?php selected( $instance['currency'], 'RUB' ); ?> >RUB</option>
 	</select>
+
+	<select name="<?php echo $this->get_field_name( 'currency1' ); ?>" id="<?php echo $this->get_field_id( 'title' ); ?>">
+  		<option <?php selected( $instance['currency1'], 'EUR' ); ?> >EUR</option>
+  		<option <?php selected( $instance['currency1'], 'RUB' ); ?> >RUB</option>
+  		<option <?php selected( $instance['currency1'], 'USD' ); ?> >USD</option>
+  		<option <?php selected( $instance['currency1'], '' ); ?> >''</option>		
+	</select>
+
 	<?php
 }
 
@@ -64,7 +73,7 @@ public function update( $new_instance, $old_instance ) {
 	// Update values
 	$insistance['title'] = strip_tags( stripslashes( $new_instance['title'] ) );
 	$insistance['currency'] = strip_tags( stripslashes( $new_instance['currency'] ) );
-
+	$insistance['currency1'] = strip_tags( stripslashes( $new_instance['currency1'] ) );
 	return $insistance;
 }
 
@@ -73,9 +82,9 @@ public function update( $new_instance, $old_instance ) {
 */
 public function widget( $args, $instance ) {
 	extract( $args );
-
 	$title = apply_filters( 'widget_title', $instance['title'] );
 	$currency = $instance['currency'];
+	$currency1 = $instance['currency1'];
 	// Display the
 	//markup before the widget ( as defined in functions.php )
 	echo $before_widget;
@@ -91,6 +100,14 @@ public function widget( $args, $instance ) {
 			$buy = $currency_array['organizations'][0]['currencies'][$currency]['ask'];
 			$sale =  $currency_array['organizations'][0]['currencies'][$currency]['bid'];
 			printf('<div><h4 class="text-center">Валюта:%s</h4><span>Продаж: %s</span><br><span>Купівля: %s</span><div>',$currency,$buy,$sale);	
+	endif; 
+	if ( $currency1 ): ?>
+		<!-- PROext: Currency Imformer Begin --> <?php
+			$currency_array=json_decode(file_get_contents("http://resources.finance.ua/ru/public/currency-cash.json"),true) ;
+			//print_r($currency_array);
+			$buy1 = $currency_array['organizations'][0]['currencies'][$currency1]['ask'];
+			$sale1 =  $currency_array['organizations'][0]['currencies'][$currency1]['bid'];
+			printf('<div><h4 class="text-center">Валюта:%s</h4><span>Продаж: %s</span><br><span>Купівля: %s</span><div>',$currency1,$buy1,$sale1);	
 	endif; 
 		echo $after_widget;
 		}
