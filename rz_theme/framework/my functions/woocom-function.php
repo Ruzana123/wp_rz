411eca65d56b9a546afda108f33b9d15d11b4c30
Ruzana123 <Ruzana18@bigmir.net>
2016-05-24 17:52:52 +0300

    test

--- rz_theme/framework/my functions/woocom-function.php
+++ rz_theme/framework/my functions/woocom-function.php
@@ -11,7 +11,7 @@ if ( ! function_exists( 'rz_sorting_products' ) ) {
         <div class="isotope" style="text-align:center;">
             <h4>Sort products by category of this page:</h4>
             <ul id="filter" class="clearfix">
-                <li style="display:inline-block;"><a href="" class="current btn" data-filter="*">Default sorting</a></li>
+                <li style="display:inline-block;"><a href="" class="current btn" data-filter="*"><?php echo __('Default sorting','rz_theme') ?></a></li>
                 <?php foreach ($all_categories as $cat) { ?>
                     <li style="display:inline-block;"><a href="" class="btn" data-filter=".product-cat-<?php echo mb_strtolower($cat->name); ?>"><?php echo $cat->name ?></a></li>
                 <?php } ?>
@@ -21,5 +21,12 @@ if ( ! function_exists( 'rz_sorting_products' ) ) {
         <?php
     }
     add_action( 'woocommerce_before_main_content', 'rz_sorting_products' );
+}
+
+
+
+function alpha_get_options($arg1){
+    global $rz_redux_demo;
+    return $rz_redux_demo[$arg1];
 }
  ?>
